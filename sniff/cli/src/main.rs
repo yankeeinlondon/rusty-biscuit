b2962e6b7d29579f49decf459b0ebd11cdbbf470
Ken Snyder <ken@ken.net>
2026-04-12 15:34:02 -0700

    feat(sniff-cli): add CliInstallUi event/prompt adapter

--- sniff/cli/src/main.rs
+++ sniff/cli/src/main.rs
@@ -2,6 +2,7 @@ mod args;
 mod commands;
 mod install;
 mod install_plan_cmd;
+mod install_ui;
 mod output;
 
 use tracing_subscriber::{filter::EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
