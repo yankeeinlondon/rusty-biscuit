b486e1ba6f3193fd5de598e00e9c1ca31d28c388
Ken Snyder <ken@ken.net>
2026-05-04 21:40:24 -0700

    chore(biscuit-terminal): update prelude exports
    - update prelude.rs to export newly added modules

--- biscuit-terminal/lib/src/prelude.rs
+++ biscuit-terminal/lib/src/prelude.rs
@@ -21,7 +21,7 @@ pub use crate::components::renderable::{BrowserRenderable, Renderable, Renderabl
 pub use crate::components::section::{HeadingLevel, Section};
 pub use crate::components::status::{Status, StatusState, StatusTheme};
 pub use crate::components::status_block::StatusBlock;
-pub use crate::components::table::table::{
+pub use crate::components::table::{
     MeasuredColumn, Table, TableCellContent, TableColumn, TableWidthError, TableWidthMeasurements,
     TableWidthPlan,
 };
