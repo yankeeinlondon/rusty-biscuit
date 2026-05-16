e853be0950e6a2b4d6377ea594d316de069a53d9
Ken Snyder <ken@ken.net>
2026-05-15 17:16:10 -0700

    refactor(renderable): update lib.rs module structure

--- renderable/src/lib.rs
+++ renderable/src/lib.rs
@@ -1,9 +1,13 @@
 pub mod ast;
 pub mod ast_utils;
 pub mod browser;
-pub mod browser_utils;
+pub mod color;
+pub mod layout;
 pub mod markdown;
 pub mod microdata;
+pub mod prelude;
+pub mod stylesheet;
 pub mod target;
+pub mod wrap_policy;
 
 pub mod html;
