ffa017d80edaea5e3da708bd4a56cecea5a1aa6c
Ken Snyder <ken@ken.net>
2026-05-22 23:33:31 -0700

    feat(darkmatter): HyperlinkStyle and ImageStyle with local_style

--- darkmatter/lib/src/style/schema/mod.rs
+++ darkmatter/lib/src/style/schema/mod.rs
@@ -5,10 +5,12 @@
 
 pub mod common;
 pub mod components;
+pub mod inline;
 pub mod lists;
 pub mod page;
 
 pub use common::CommonStyle;
 pub use components::{BlockQuoteStyle, TableStyle};
+pub use inline::{HyperlinkStyle, ImageStyle};
 pub use lists::{LiStyle, OlStyle, UlStyle};
 pub use page::{CodeStyle, PageStyle};
