164bc742ac7cf10997ad6a83ddac493af94d06d0
Ken Snyder <ken@ken.net>
2026-05-22 23:26:41 -0700

    feat(darkmatter): TableStyle and BlockQuoteStyle

--- darkmatter/lib/src/style/schema/mod.rs
+++ darkmatter/lib/src/style/schema/mod.rs
@@ -4,7 +4,9 @@
 //! structs live in sibling files.
 
 pub mod common;
+pub mod components;
 pub mod page;
 
 pub use common::CommonStyle;
+pub use components::{BlockQuoteStyle, TableStyle};
 pub use page::{CodeStyle, PageStyle};
