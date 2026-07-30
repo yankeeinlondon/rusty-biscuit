fbf8692232980ea6b214c48aaee18690c116f38e
Ken Snyder <ken@ken.net>
2026-07-30 00:42:34 -0700

    fix(sniff): remove forced duplicate linking

--- sniff/cli/build.rs
+++ sniff/cli/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("msvc") {
-        println!("cargo::rustc-link-arg=/FORCE:MULTIPLE");
         println!("cargo::rustc-link-arg-bins=/STACK:33554432");
     }
 }
